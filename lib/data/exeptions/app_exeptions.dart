222c9ede805837c50550cdd4e3467017f3d4d06e
tarik <mdtarikbinaziz@gmail.com>
2025-07-13 15:24:09 +0600

    add exceptions

--- lib/data/exeptions/app_exeptions.dart
+++ lib/data/exeptions/app_exeptions.dart
@@ -1,7 +1,15 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
 class AppExeptions implements Exception {
-  final String message;
-  final String prefix;
-  AppExeptions(this.message);
+  final String? message;
+  final String? prefix;
+  AppExeptions([this.message, this.prefix]);
+
+  @override
+  String toString() {
+    return "$message$prefix";
+  }
+}
+
+class NoInternetException extends AppExeptions {
+  NoInternetException([String? message])
+      : super(message, "No Internet Connection");
 }
