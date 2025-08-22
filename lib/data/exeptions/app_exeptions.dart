1cd5f47b37796d9033a0066de7ea8a5e4e2e562e
tarikbinaziz <mdtarikbinaziz@gmail.com>
2025-08-22 23:27:46 +0600

    add dio

--- lib/data/exeptions/app_exeptions.dart
+++ lib/data/exeptions/app_exeptions.dart
@@ -14,7 +14,8 @@ class FetchDataException extends AppException {
   /// Constructor for creating a [FetchDataException] instance.
   ///
   /// The [message] parameter represents the error message.
-  FetchDataException([String? message]) : super(message, 'Error During Communication');
+  FetchDataException([String? message])
+      : super(message, 'Error During Communication');
 }
 
 /// Exception class representing a bad request error.
@@ -30,7 +31,8 @@ class UnauthorisedException extends AppException {
   /// Constructor for creating an [UnauthorisedException] instance.
   ///
   /// The [message] parameter represents the error message.
-  UnauthorisedException([String? message]) : super(message, 'Unauthorised request');
+  UnauthorisedException([String? message])
+      : super(message, 'Unauthorised request');
 }
 
 /// Exception class representing an invalid input error.
@@ -41,10 +43,27 @@ class InvalidInputException extends AppException {
   InvalidInputException([String? message]) : super(message, 'Invalid Input');
 }
 
+/// Exception class representing a not found error.
+class NotFoundException extends AppException {
+  /// Constructor for creating a [NotFoundException] instance.
+  ///
+  /// The [message] parameter represents the error message.
+  NotFoundException([String? message]) : super(message, 'Not Found');
+}
+
 /// Exception class representing a no internet connection error.
 class NoInternetException extends AppException {
   /// Constructor for creating a [NoInternetException] instance.
   ///
   /// The [message] parameter represents the error message.
-  NoInternetException([String? message]) : super(message, 'No Internet Connection');
+  NoInternetException([String? message])
+      : super(message, 'No Internet Connection');
+}
+
+class InternalServerException extends AppException {
+  /// Constructor for creating an [InternalServerException] instance.
+  ///
+  /// The [message] parameter represents the error message.
+  InternalServerException([String? message])
+      : super(message, 'Internal Server Error');
 }
--- lib/data/network/network_services_api.dart
+++ lib/data/network/network_services_api.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:asiftaj_bloc_clean_code/data/exeptions/app_exeptions.dart';
 import 'package:asiftaj_bloc_clean_code/data/network/base_api_services.dart';
 import 'package:http/http.dart' as http;
@@ -82,3 +81,118 @@ dynamic returnResponse(http.Response response) {
       throw FetchDataException("Something went wrong, please try again");
   }
 }
+
+
+// With dio 
+
+// class NetworkServicesApi implements BaseApiServices {
+//   final Dio _dio = Dio(
+//     BaseOptions(
+//       connectTimeout: const Duration(seconds: 10),
+//       receiveTimeout: const Duration(seconds: 15),
+//       sendTimeout: const Duration(seconds: 10),
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json',
+//       },
+//     ),
+//   );
+
+//   @override
+//   Future<dynamic> getApi(String url) async {
+//     try {
+//       final response = await _dio.get(url);
+//       return _handleResponse(response);
+//     } on DioException catch (e) {
+//       throw _handleDioError(e);
+//     } on SocketException {
+//       throw NoInternetException("No Internet connection");
+//     }
+//   }
+
+//   @override
+//   Future<dynamic> postApi(String url, dynamic data) async {
+//     try {
+//       final response = await _dio.post(url, data: data);
+//       return _handleResponse(response);
+//     } on DioException catch (e) {
+//       throw _handleDioError(e);
+//     } on SocketException {
+//       throw NoInternetException("No Internet connection");
+//     }
+//   }
+
+//   @override
+//   Future<dynamic> putApi(String url, dynamic data) async {
+//     try {
+//       final response = await _dio.put(url, data: data);
+//       return _handleResponse(response);
+//     } on DioException catch (e) {
+//       throw _handleDioError(e);
+//     } on SocketException {
+//       throw NoInternetException("No Internet connection");
+//     }
+//   }
+
+//   @override
+//   Future<dynamic> deleteApi(String url) async {
+//     try {
+//       final response = await _dio.delete(url);
+//       return _handleResponse(response);
+//     } on DioException catch (e) {
+//       throw _handleDioError(e);
+//     } on SocketException {
+//       throw NoInternetException("No Internet connection");
+//     }
+//   }
+
+//   /// Handle successful response
+//   dynamic _handleResponse(Response response) {
+//     if (response.statusCode != null &&
+//         response.statusCode! >= 200 &&
+//         response.statusCode! < 300) {
+//       return response.data;
+//     } else {
+//       throw FetchDataException(
+//           "Unexpected status code: ${response.statusCode}");
+//     }
+//   }
+
+//   /// Map Dio errors to custom App Exceptions
+//   AppException _handleDioError(DioException error) {
+//     switch (error.type) {
+//       case DioExceptionType.connectionTimeout:
+//       case DioExceptionType.sendTimeout:
+//       case DioExceptionType.receiveTimeout:
+//         return FetchDataException("Request timeout, please try again");
+
+//       case DioExceptionType.badResponse:
+//         final statusCode = error.response?.statusCode;
+//         final message = error.response?.data?['message'] ??
+//             "Something went wrong (${error.response?.statusCode})";
+
+//         if (statusCode == 400) {
+//           return BadRequestException(message);
+//         } else if (statusCode == 401 || statusCode == 403) {
+//           return UnauthorisedException(message);
+//         } else if (statusCode == 404) {
+//           return NotFoundException(message);
+//         } else if (statusCode == 500) {
+//           return InternalServerException(message);
+//         }
+//         return FetchDataException(message);
+
+//       case DioExceptionType.cancel:
+//         return FetchDataException("Request cancelled");
+
+//       case DioExceptionType.unknown:
+//         if (error.error is SocketException) {
+//           return NoInternetException("No Internet connection");
+//         }
+//         return FetchDataException("Unexpected error occurred");
+
+//       default:
+//         return FetchDataException("Something went wrong");
+//     }
+//   }
+// }
